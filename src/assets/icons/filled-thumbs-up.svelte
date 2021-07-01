8042c0c3533e328c3176d2564477e59993c2fb55
Ricardo Delgado <ricardo.delgado@penn-interactive.com>
2021-07-01 10:31:59 -0400

    like button

--- src/assets/icons/filled-thumbs-up.svelte
+++ src/assets/icons/filled-thumbs-up.svelte
@@ -1,13 +1,47 @@
-<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
-    <g fill="none" fill-rule="evenodd">
-        <g fill="currentColor">
-            <g>
+<script>
+    export let increaseCount
+
+    const click = () => {
+        selected = !selected
+        increaseCount()
+    }
+
+    let selected = false
+</script>
+
+<span class="tu-icon {selected ? 'selected' : 'deselected'}" on:click={click}>
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
+        <g fill="none" fill-rule="evenodd">
+            <g fill={selected ? "#C58E01" : "#d6d6d6" }>
                 <g>
                     <g>
-                        <path d="M14 6H8V2c0-1.2-.8-2-2-2L3 7H1c-.6 0-1 .4-1 1v7c0 .6.4 1 1 1h11c1.4 0 2.7-1 2.9-2.4l1-5.2C16.2 7.2 15.3 6 14 6z" transform="translate(-32.000000, -389.000000) translate(16.000000, 250.000000) translate(16.000000, 138.000000) translate(-0.000000, 1.000000)"/>
+                        <g>
+                            <path d="M14 6H8V2c0-1.2-.8-2-2-2L3 7H1c-.6 0-1 .4-1 1v7c0 .6.4 1 1 1h11c1.4 0 2.7-1 2.9-2.4l1-5.2C16.2 7.2 15.3 6 14 6z" transform="translate(-32.000000, -389.000000) translate(16.000000, 250.000000) translate(16.000000, 138.000000) translate(-0.000000, 1.000000)"/>
+                        </g>
                     </g>
                 </g>
             </g>
         </g>
-    </g>
-</svg>
+    </svg>
+</span>
+
+<style>
+    .selected {
+        animation: bump 300ms;
+        transform: scale(1);
+    }
+
+    @keyframes bump {
+        from {
+            transform: scale(1);
+        }
+
+        50% {
+            transform: scale(1.2);
+        }
+
+        to {
+            transform: scale(1);
+        }
+    }
+</style>
--- src/components/feedCards/components/ThumbsUpFooter.svelte
+++ src/components/feedCards/components/ThumbsUpFooter.svelte
@@ -1,50 +1,17 @@
 <script>
   import FilledThumbsUpIcon from '../../../assets/icons/filled-thumbs-up.svelte'
-
-  export let selected = false
-
-  const click = () => {
-    selected = !selected
-  }
 </script>
 
-<div class="thumbs-up" on:click={click}>
-  <div class={selected ? 'selected' : ''}>
-    <FilledThumbsUpIcon class="thumbs-up-icon" />
-  </div>
+<div class="thumbs-up">
+  <FilledThumbsUpIcon class="thumbs-up-icon" />
 
   <slot name="count"></slot>
 </div>
 
 <style>
   .thumbs-up {
-    --tu-icon-color: #d6d6d6;
-  }
-  .selected {
-    --tu-icon-color: #C58E01;
-  }
-  .thumbs-up {
-    color: var(--tu-icon-color);
     cursor: pointer;
     display: flex;
     gap: 8px;
-  }
-
-  .selected {
-    animation: bump 300ms;
-  }
-
-  @keyframes bump {
-    from {
-      transition: scale(1);
-    }
-
-    50% {
-      transition: scale(1.3);
-    }
-
-    to {
-      transition: scale(1);
-    }
   }
 </style>
